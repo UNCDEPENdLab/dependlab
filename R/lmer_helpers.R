b88ebf03624506d70126bbb45dfad78e24350509
Nate Hall <nate.hall329@gmail.com>
2020-12-04 14:06:14 -0500

    does this help?

--- R/lmer_helpers.R
+++ R/lmer_helpers.R
@@ -1,6 +1,3 @@
-# Helper functions for lmer model building and comparison -----------------
-## written by MNH with minor tweaks from NTH
-
 #' Render a pretty AIC comparison table in flextable for iterative model building and manuscrips.
 #'
 #' @param minfo named character vector containing model infomation to print to the table. (e.g. fixed and random effects)
@@ -28,19 +25,3 @@ render_aictab <- function(minfo, mobs =NULL, ...) {
 }
 
 
-# implement in a sec.
-#
-# flextb_vif <- function(m) {
-#   flextable(data.frame(vif=car::vif(m)) %>% rownames_to_column("Predictor")) %>% autofit()
-# }
-#
-# check_singularity <- function(lmerobj) {
-#   tt <- getME(lmerobj,"theta") #RE estimates
-#   ll <- getME(lmerobj,"lower") #lower bound on estimate
-#
-#   #look for RE estimates that are very small (near zero) and the lower bound encompasses 0
-#   low_re <- tt[ll==0 & tt < 1e-2]
-#   return(low_re)
-#
-#   #eventually, would be nice to provide functionality to drop these from model and refit automatically
-# }
