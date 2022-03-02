32b02580777beb4e8a56ad0cca9c1c7b5b48b0be
Nate Hall <nate.hall329@gmail.com>
2022-03-02 16:08:33 -0500

    add setup_study and documentation

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // generate_feature
 NumericMatrix generate_feature(NumericVector encoding, int K);
 RcppExport SEXP _dependlab_generate_feature(SEXP encodingSEXP, SEXP KSEXP) {
